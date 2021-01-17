5acc4ca62620b381efc4e89618ec86d53649d3ac
Alexis <alcamoru@gmail.com>
2021-01-17 08:53:44 +0100

    Finished project

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,33 +1,52 @@
 import random
+
 # Write your code here
-LIVES = 8
 
-CHOICES = ["python", "java", "kotlin", "javascript"]
-print("H A N G M A N")
-CHOICE = list(random.choice(CHOICES))
+user_action = input('Type "play" to play the game, "exit" to quit:')
 
-hidden_word = list(len(CHOICE) * "-")
+if user_action == "play":
+    LIVES = 8
 
-while LIVES > 0:
-    print("\n" + "".join(hidden_word))
-    user_input = input("Input a letter: ")
+    CHOICES = ["python", "java", "kotlin", "javascript"]
+    print("H A N G M A N")
+    CHOICE = list(random.choice(CHOICES))
 
-    if user_input in CHOICE:
-        for i in range(len(CHOICE)):
-            if CHOICE[i] == user_input:
-                if hidden_word[i] == user_input:
-                    print("No improvements")
-                    LIVES -= 1
-                    break
-                else:
-                    hidden_word[i] = user_input
+    hidden_word = list(len(CHOICE) * "-")
+    attempts = []
+
+    while LIVES > 0:
+        if hidden_word == list(CHOICE):
+            break
+        print("\n" + "".join(hidden_word))
+        user_input = input("Input a letter: ")
+        if len(user_input) > 1:
+            print("You should input a single letter")
+            continue
+        elif not user_input.isalpha() or user_input.isupper():
+            print("Please enter a lowercase English letter")
+            continue
+
+        if user_input in CHOICE:
+            for i in range(len(CHOICE)):
+                if CHOICE[i] == user_input:
+                    if hidden_word[i] == user_input:
+                        print("You've already guessed this letter")
+                        break
+                    else:
+                        hidden_word[i] = user_input
+
+        else:
+            if user_input in attempts and user_input not in CHOICE:
+                print("You've already guessed this letter")
+            else:
+                print("That letter doesn't appear in the wor"
+                      "d")
+                LIVES -= 1
+                attempts.append(user_input)
+
+    if "-" not in hidden_word:
+        print(f"""{CHOICE}
+    You guessed the word!
+    You survived!""")
     else:
-        print("That letter doesn't appear in the word")
-        LIVES -= 1
-
-if "-" not in hidden_word:
-    print(f"""{CHOICE}
-You guessed the word!
-You survived!""")
-else:
-    print("You lost!")
+        print("You lost!")
